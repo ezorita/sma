f819f4e0e848e0be73cbe48fbc9a237285e75a42
Eduard Valera i Zorita <eduardvalera@gmail.com>
2014-05-23 16:08:27 +0200

    First version. Could be improved by avoiding suffix mismatch recomputation.

--- sma.c
+++ sma.c
@@ -1,76 +1,274 @@
 #include "sma.h"
 
 // TODO:
-// - When increasing index, check whether the num of remaining nucleotides is less than the available tau,
-//   in this case do not continue beacuse it's not possible to create seqs of distance tau.
-// - Decrease the sequence len when inserting unknown nucleotides after a deletion!! Otherwise there'll be
-//   repeated sequences! (a param to count the accumulated 'unkn' may be necessary).
+// - Reuse suffixes to avoid recomputing all the suffix mismatches.
+// - Aixo es podria fer desant un index de l'ultima vegada que s'ha entrat cert nivell
+//   de start i quan s'hi torni a entrar tirar tantes seqs endarrera i copiar-les totes
+//   substituint nomes la part del prefix, sense haver de fer tota la computacio recursiva
+//   una altra vegada.
 
-ustack_t * sma (uint seq, uint slen, unsigned char tau) {
-   
+
+void
+print_stack
+(
+ mstack_t * stack,
+ int slen
+ )
+{
+   for (int k = 0; k < stack->pos; k++) {
+      char * seq = idtoseq(stack->m[k].seq, slen);
+      fprintf(stdout,"%s\n", seq);
+      free(seq);
+   }
 }
 
+void
+sma
+(
+ mstack_t ** mstack,
+ seq_t       seq,
+ seq_t       slen,
+ char      * extra,
+ int         tau
+)
 
-void fill_params (param_t * params, seq_t seq, seq_t extra, seq_t extracnt) {
-   
-}
+{
+   if (mstack[tau]->seq == seq) return;
 
-void rec_sma (seq_t seq, int slen, int start, int tau, int offset, param_t params, seq_t *count) {
-
-   // Write sequence if tau is exhausted.
+   // Clear stack.
+   mstack[tau]->pos = 0;
    if (tau == 0) {
-      stack_add(seq, params.seqstack + offset);
+      add_mismatch(mstack, seq, 0);
       return;
-   } 
+   }
 
-   // Return when it's not possible to create seqs of distance tau anymore.
-   if (slen - start < tau) return;
-   
-   // Create a mask for the current nucleotide.
-   unsigned char pos = 2*(slen - start - 1);
-   seq_t ntmask = 3 << pos;
-   seq_t inc    = 1 << pos;
+   // Check whether the previous has been computed.
+   if (mstack[tau-1]->seq != seq) sma(mstack, seq, slen, extra, tau-1);
 
-   // Iterate:
-   do {
-      // Go one level deeper instead of substituting for the same nucleotide.
-      if ((seq & ntmask) == ((*count) & ntmask)) {
-         rec_sma(seq, slen, start+1, tau, offset, params, count);
-      }
-      // The new nucleotide is not the same as the original.
-      else {
-         // If this is the last nt, only substitute.
-         if (start == slen - 1) {
-            stack_add(sub_nt(seq, pos, (*count)), params.seqstack + offset);
-         }
-         // The new nucleotide is different than the next one as well: Just insert/substitute.
-         else if (((seq << 2) & ntmask) != ((*count) & ntmask)) {
-            // If subsequence is positive: INS -> SUB.
-            if (params.sign & inc) {
-               // INS. (offset + 1 to count the insertion)
-               seq_t subcount = 0;
-               rec_sma(ins_nt(seq, pos, (*count)), slen, start + 1, tau - 1, offset + 1, params, &subcount);
-               // SUB.
-               subcount = 0;
-               rec_sma(sub_nt(seq, pos, (*count)), slen, start + 1, tau - 1, offset, params, &subcount);
-            }
-            // If subsequence is negative: SUB -> INS.
-            else {
-               // SUB.
-               seq_t subcount = 0;
-               rec_sma(sub_nt(seq, pos, (*count)), slen, start + 1, tau - 1, offset, params, &subcount);
-               // INS. (offset + 1 to count the insertion)
-               subcount = 0;
-               rec_sma(ins_nt(seq, pos, (*count)), slen, start + 1, tau - 1, offset + 1, params, &subcount);
-            }
-         }
-         // The new nucleotide is the same as the next one: Time for deletions!
+   // Run recursive generator.
+   struct param_t params = {
+      .slen = slen,
+      .extra = extra,
+      .mstack = mstack + tau
+   };
+   sma_gen(seq, 0, tau, 0, params, 0);
+
+   // Sort.
+   mstack[tau] = realloc(mstack[tau], 3*sizeof(seq_t) + mstack[tau]->pos*sizeof(mismatch_t));
+   mstack[tau]->lim = mstack[1]->pos;
+   qsort(mstack[tau]->m, mstack[tau]->pos, sizeof(mismatch_t), mcomp);
+
+   sma_merge(mstack, tau);
+}
+
+
+void
+sma_merge
+(
+ mstack_t ** mstackp,
+ int         tau
+)
+// SYNOPSIS:                                                              
+//   Merges a sorted stack containing mismatches of distance leq than tau with all the
+//   previously-merged stacks of distance tau-1, tau-2, ... 0 from the original sequence.
+//   As a result of the merge, the stack contains only sequences that are exactly at
+//   distance tau from the original seq. For the algorithm to work, all the merged
+//   stacks of distance leq than the specified tau must be present at the stack array.
+//   Merging a previously merged stack returns exactly the same result.
+//
+// PARAMETERS:                                                            
+//   mstackp: a pointer to the beginning of the stack array.
+//   tau:     the stack that will be merged.
+//                                                                        
+// RETURN:                                                                
+//   The function does not return anything. The merged list of mismatches is stored in the
+//   same original stack. The final stack is realloc'ed to fit exactly the final size of the
+//   set.
+//                                                                        
+// SIDE EFFECTS:                                                          
+//   The tau-th stack of the array may be realloc'ed.
+//   
+{
+   // Get the pointer.
+   mstack_t   * dst_stack = mstackp[tau];
+   seq_t        dst_sz    = dst_stack->pos;
+   mismatch_t * dst_lst   = dst_stack->m;
+
+   // Remove duplicates first.
+   int p = 0;
+   for (int i = 0; i < dst_sz - 1; i++)
+      if (dst_lst[i].seq != dst_lst[i+1].seq) dst_lst[p++] = dst_lst[i];
+
+   dst_lst[p++] = dst_lst[dst_sz - 1];
+   // Save new size.   
+   dst_stack->pos = p;
+
+   // Merge the current stack with all the previous.
+   for (int i = 0; i < tau; i++) {
+      mstack_t   * ref_stack = mstackp[i];
+      seq_t        ref_sz    = ref_stack->pos;
+      mismatch_t * ref_lst   = ref_stack->m;
+
+      int j = 0, k = 0, l = 0;
+      while (j < ref_sz && k < dst_sz) {
+         if (ref_lst[j].seq < dst_lst[k].seq)
+            j++;
+         else if (ref_lst[j].seq > dst_lst[k].seq) 
+            dst_lst[l++] = dst_lst[k++];
          else {
-            
+            j++; k++;
          }
       }
-      (*count) += inc;
-    
-   } while (((*count) & ntmask) < ntmask);
+
+      // Complete copy.
+      if (k < dst_sz) 
+         memmove(dst_lst + l, dst_lst+ k, (dst_sz - k)*sizeof(mismatch_t));
+
+      // Save the final size.
+      dst_sz -= k-l;
+   }
    
+   dst_stack->pos = dst_sz;
+
+   // Job done, realloc destination stack.
+   dst_stack->lim = dst_stack->pos;
+   mstackp[tau] = realloc(dst_stack, 3*sizeof(seq_t) + dst_stack->pos*sizeof(mismatch_t));
+   if (mstackp[tau] == NULL) {
+      fprintf(stderr, "Error (realloc) in sma_merge.\n");
+      exit(EXIT_FAILURE);
+   }
+}
+
+
+void
+sma_gen
+(
+ seq_t   seq,
+ int     start,
+ char    tau,
+ char    offset,
+ param_t params,
+ char    last
+)
+// SYNOPSIS:                                                              
+//   Recursively generates all INS/SUB/DEL possible combinations up to tau mismatches.
+//   The resulting set may contain sequences within distance < tau wrt the original seq,
+//   due to redundant mismatches.
+//   Only straightforward redundant mismatches are avoided:
+//     - Inserting/deleting within repeated nucleotides.
+//     - Del+Ins and Ins+Del.
+//     - Inserting deleting with a constant suffix.
+//     - delete then substitute by the previous (same as deleting the next).
+//
+// PARAMETERS:                                                            
+//   seq:    the sequence id.
+//   start:  start level (nucleotide).
+//   tau:    remaining num of mismatches.
+//   offset: offset of the current sequence. (effective num of dels or ins).
+//   params: parameter struct.
+//   last:   informs about the last action, set to 0. (used in recursive generation).
+//                                                                        
+// RETURN:                                                                
+//   The set of sequences is returned in the mismatch stack provided in params.
+//                                                                        
+// SIDE EFFECTS:                                                          
+//   The mismatch stack may be realloc'ed.
+//   
+{
+   // Tau exhausted, save seq and return.
+   if (tau == 0) {
+      // Remember to clear the upper bits!
+      add_mismatch(params.mstack, seq & ~(MAXVAL << (2*params.slen)), offset);
+      return;
+   }
+
+   // End of chain
+   if (start == params.slen) return;
+
+   // Create a mask for the current nucleotide.
+   unsigned char pos = 2*(params.slen - start - 1);
+   seq_t ntmask = 3 << pos;
+
+   // Count.
+   char base = (seq >> pos) & 3;
+   char next = (seq >> (pos - 2)) & 3;
+
+   // SUBSTITUTIONS:
+   char deleted = (last < 0 ? -last-1 : -1);
+   for (int i = 0; i < 4; i++) {
+      if (i == base || i == deleted) continue;
+      sma_gen(sub_nt(seq, (seq_t)pos, (seq_t)i), start+1, tau-1, offset, params, 0);
+   }
+
+   if (start < params.slen - 1) {
+      // DELETIONS: (Only if q[i] != q[i+1] and previous was not an insertion)
+      if ((base != next) && del_check(seq, pos, params.extra[offset]) && last <= 0) {
+         if (params.extra[offset] < 0) {
+            for (int i = 0; i < 4; i++) {
+               sma_gen(del_nt(seq, (seq_t)pos, (seq_t)i), start, tau-1, offset-1, params, -base-1);
+            }
+         }
+         else {
+            sma_gen(del_nt(seq, (seq_t)pos, (seq_t)params.extra[offset]), start, tau-1, offset-1, params, -base-1);
+         }
+      }
+   
+      // INSERTIONS:
+      if (ins_check(seq, pos) && (last >= 0)) {
+         for (int i = 0; i < 4; i++) {
+            if (i == base) continue;
+            sma_gen(ins_nt(seq,(seq_t)pos, (seq_t)i), start+1, tau-1, offset+1, params, base + 1);
+         }
+      }
+   }
+
+   // Continue w/ next nucleotide.
+   sma_gen(seq, start+1, tau, offset, params, 0);
+}
+
+void
+add_mismatch
+(
+ mstack_t ** stackp,
+ seq_t       seq,
+ char        offset
+)
+{
+   mstack_t * mstack = *stackp;
+   
+   if (mstack->pos >= mstack->lim) {
+      size_t newsize = mstack->lim * 2;
+      mstack_t * p = realloc(mstack, 3*sizeof(seq_t) + newsize * sizeof(mismatch_t));
+      if (p == NULL) {
+         fprintf(stderr, "mismatch realloc failed: %s", strerror(errno));
+         exit(EXIT_FAILURE);
+      }
+      *stackp = mstack = p;
+      mstack->lim = newsize;
+   }
+
+   struct mismatch_t mismatch = {
+      .offset = offset,
+      .seq    = seq
+   };
+
+   mstack->m[mstack->pos++] = mismatch;
+}
+
+
+int
+mcomp
+(
+ const void * a,
+ const void * b
+ )
+{
+   mismatch_t * ma = (mismatch_t *) a;
+   mismatch_t * mb = (mismatch_t *) b;
+   if (ma->seq > mb->seq)
+      return 1;
+   else if (ma->seq < mb->seq)
+      return -1;
+   else
+      return 0;
 }
--- sma.h
+++ sma.h
@@ -1,35 +1,48 @@
+#include "plutocore.h"
+#include <execinfo.h>
+#include <signal.h>
 
-
-
-#ifndef __SMA_
+#ifndef __SMA__
 #define __SMA__
 
+
 #define MAXVAL 0xFFFFFFFF
-#define sub_nt(seq, pos, base) ((seq & ~(3 << pos)) + (base & (3 << pos))
-#define ins_nt(seq, pos, base) ((seq & (MAXVAL << pos)) + ((seq & ~(MAXVAL << pos)) >> 2) + (base & (3 << pos)))
+
+#define sub_nt(seq, pos, base)  ((seq & ~(3 << pos)) + ((base & 3) << pos))
+#define ins_nt(seq, pos, base)  ((seq & (MAXVAL << (pos + 2))) + ((seq >> 2) & ~(MAXVAL << pos)) + ((base & ~(MAXVAL << 2)) << pos))
+#define del_nt(seq, pos, extra) ((seq & (MAXVAL << (pos + 2))) + ((seq << 2) & ~(MAXVAL << (pos + 2))) + (extra & 3))
+#define get_suffix(seq, len)    (seq & ~(MAXVAL << pos))
+#define ins_check(seq, pos)     (get_suffix(seq, pos - 2) != get_suffix(seq >> 2, pos - 2))
+#define del_check(seq, pos, extra) (get_suffix(seq, pos - 2) != get_suffix(del_nt(seq, pos, extra), pos - 2))
 
 typedef unsigned int seq_t; // Longest seq: 16 nt (32 bits)
-typedef struct ustack_t ustack_t;
+typedef struct mstack_t mstack_t;
 typedef struct param_t  param_t;
+typedef struct mismatch_t mismatch_t;
 
-struct seqstack_t {
-   seq_t pos;
-   seq_t lim;
-   seq_t seq[];
+struct mismatch_t {
+   char  offset;
+   seq_t seq;
+};
+
+struct mstack_t {
+   seq_t        seq;
+   seq_t        pos;
+   seq_t        lim;
+   mismatch_t   m[];
 };
 
 struct param_t {
-   seq_t         sign;
-   seq_t         extra;
-   int           extracnt;
-   seqstack_t ** seqstack;
-   // sign:
-   //   bit 0: subseq sign (0 neg, 1 pos)
-   //   bit 1: 2-subseq sign (0 neg, 1 pos)
-   // extra: (used for deletions)
-   //   Known nucleotides (count and seq) that follow the sequence.
+   char        slen;
+   char      * extra;
+   mstack_t ** mstack;
 };
 
-void fill_params(param_t *params, seq_t seq, seq_t extra, int extracnt);
+void sma (mstack_t **, seq_t, seq_t, char *, int);
+void sma_gen (seq_t, int, char, char, param_t, char);
+void sma_merge (mstack_t **, int);
+void add_mismatch (mstack_t **, seq_t, char);
+int  mcomp (const void *, const void *);
+void print_stack (mstack_t *, int);
 
 #endif
